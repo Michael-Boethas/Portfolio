952c863c3445a0d22e843eac3d897fe7834dcf32
Michael-Boethas <68383329+Michael-Boethas@users.noreply.github.com>
2024-10-24 20:26:57 +0200

    WIP Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,16 @@
+import Image from "next/image";
+
 export default function Footer() {
   return (
     <footer className="footer">
       <p>FOOTER</p>
+      <Image
+            src="/images/ocr_logo.webp"
+            alt="Logo Openclassroom"
+            width={50}
+            height={50}
+            priority
+          />
     </footer>
   );
 }
